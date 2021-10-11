8b42b0db6edceda66ab8150eb7849a8bfdf55e2d
shawnreid <58261727+shawnreid@users.noreply.github.com>
2021-10-11 19:16:22 -0300

    .

--- app/Services/Game/TeamStatsService.php
+++ app/Services/Game/TeamStatsService.php
@@ -3,66 +3,65 @@
 namespace App\Services\Game;
 
 use App\Models\Games\Games;
-use App\Models\Games\SkaterStats;
 use App\Models\Games\TeamStats;
-use Illuminate\Support\Facades\DB;
-
 class TeamStatsService
 {
     protected array $columns = [
-        'goals',
-        'assists',
-        'points',
-        'shots',
-        'hits',
-        'pp_goals',
-        'pp_points',
-        'pim',
-        'fo_wins',
-        'fo_taken',
-        'takeaways',
-        'giveaways',
-        'sh_goals',
-        'sh_assists',
-        'sh_points',
-        'blocked_shots',
+        'goals'         => 0,
+        'assists'       => 0,
+        'points'        => 0,
+        'shots'         => 0,
+        'hits'          => 0,
+        'pp_goals'      => 0,
+        'pp_assists'    => 0,
+        'pp_points'     => 0,
+        'pim'           => 0,
+        'fo_wins'       => 0,
+        'fo_taken'      => 0,
+        'takeaways'     => 0,
+        'giveaways'     => 0,
+        'sh_goals'      => 0,
+        'sh_assists'    => 0,
+        'sh_points'     => 0,
+        'blocked_shots' => 0
     ];
 
-    public function save(Games $game, array $data): void
+    public function save(Games $game, array $data, array $res = []): void
     {
-        $results = [];
         foreach ($data['teams'] as $team) {
-            $tid = $team['team']['id'];
-            $r = &$results;
-            $r[$tid]['season_id'] = $game->season_id;
-            $r[$tid]['game_id'] = $game->id;
-            $r[$tid]['game_type_id'] = $game->game_type_id;
-            $r[$tid]['team_id'] = $tid;
+            $res[$team['team']['id']] = $this->columns;
+            $r = &$res[$team['team']['id']];
+
+            $r['season_id']    = $game->season_id;
+            $r['game_id']      = $game->id;
+            $r['game_type_id'] = $game->game_type_id;
+            $r['team_id']      = $team['team']['id'];
+            
             foreach ($team['players'] as $player) {
                 if (isset($player['stats']['skaterStats'])) {
                     $stats = $player['stats']['skaterStats'];
-                    $r[$tid]['goals']         = _s($r[$tid]['goals'], 0) + _s($stats['goals'], 0);
-                    $r[$tid]['assists']       = _s($r[$tid]['assists'], 0) + _s($stats['assists'], 0);
-                    $r[$tid]['points']        = _s($r[$tid]['points'], 0) + _s($stats['goals'], 0) + _s($stats['assists'], 0);
-                    $r[$tid]['shots']         = _s($r[$tid]['shots'], 0) + _s($stats['shots'], 0);
-                    $r[$tid]['hits']          = _s($r[$tid]['hits'], 0) + _s($stats['hits'], 0);
-                    $r[$tid]['pp_goals']      = _s($r[$tid]['pp_goals'], 0) + _s($stats['powerPlayGoals'], 0);
-                    $r[$tid]['pp_assists']    = _s($r[$tid]['pp_goals'], 0) + _s($stats['powerPlayAssists'], 0);
-                    $r[$tid]['pp_points']     = _s($r[$tid]['pp_points'], 0) + _s($stats['powerPlayGoals'], 0) + _s($stats['powerPlayAssists'], 0);
-                    $r[$tid]['pim']           = _s($r[$tid]['pim'], 0) + _s($stats['penaltyMinutes'], 0);
-                    $r[$tid]['fo_wins']       = _s($r[$tid]['fo_wins'], 0) + _s($stats['faceOffWins'], 0);
-                    $r[$tid]['fo_taken']      = _s($r[$tid]['fo_taken'], 0) + _s($stats['faceoffTaken'], 0);
-                    $r[$tid]['takeaways']     = _s($r[$tid]['takeaways'], 0) + _s($stats['takeaways'], 0);
-                    $r[$tid]['giveaways']     = _s($r[$tid]['giveaways'], 0) + _s($stats['giveaways'], 0);
-                    $r[$tid]['sh_goals']      = _s($r[$tid]['sh_goals'], 0) + _s($stats['shortHandedGoals'], 0);
-                    $r[$tid]['sh_assists']    = _s($r[$tid]['sh_assists'], 0) + _s($stats['shortHandedAssists'], 0);
-                    $r[$tid]['sh_points']     = _s($r[$tid]['sh_points'], 0) + _s($stats['shortHandedGoals'], 0) + _s($stats['shortHandedAssists'], 0);
-                    $r[$tid]['blocked_shots'] = _s($r[$tid]['blocked_shots'], 0) + _s($stats['blocked'], 0);
+                    $r['goals']         += _s($stats['goals'], 0);
+                    $r['assists']       += _s($stats['assists'], 0);
+                    $r['points']        += _s($stats['goals'], 0) + _s($stats['assists'], 0);
+                    $r['shots']         += _s($stats['shots'], 0);
+                    $r['hits']          += _s($stats['hits'], 0);
+                    $r['pp_goals']      += _s($stats['powerPlayGoals'], 0);
+                    $r['pp_assists']    += _s($stats['powerPlayAssists'], 0);
+                    $r['pp_points']     += _s($stats['powerPlayGoals'], 0) + _s($stats['powerPlayAssists'], 0);
+                    $r['pim']           += _s($stats['penaltyMinutes'], 0);
+                    $r['fo_wins']       += _s($stats['faceOffWins'], 0);
+                    $r['fo_taken']      += _s($stats['faceoffTaken'], 0);
+                    $r['takeaways']     += _s($stats['takeaways'], 0);
+                    $r['giveaways']     += _s($stats['giveaways'], 0);
+                    $r['sh_goals']      += _s($stats['shortHandedGoals'], 0);
+                    $r['sh_assists']    += _s($stats['shortHandedAssists'], 0);
+                    $r['sh_points']     += _s($stats['shortHandedGoals'], 0) + _s($stats['shortHandedAssists'], 0);
+                    $r['blocked_shots'] += _s($stats['blocked'], 0);
                 }
             }
         }
 
         TeamStats::where('game_id', $game->id)->delete();
-        TeamStats::insert($results);
+        TeamStats::insert($res);
     }
 }
