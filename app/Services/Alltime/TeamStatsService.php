ca5ee04bd802787b2c332e7a322d4d91deeb2fc7
shawnreid <58261727+shawnreid@users.noreply.github.com>
2021-10-11 19:01:39 -0300

    .

--- app/Services/Alltime/TeamStatsService.php
+++ app/Services/Alltime/TeamStatsService.php
@@ -2,10 +2,47 @@
 
 namespace App\Services\Alltime;
 
+use App\Models\Games\TeamStats as GameTeamStats;
+use App\Models\Alltime\TeamStats as AlltimeTeamStats;
+use Illuminate\Support\Facades\DB;
+
 class TeamStatsService
 {
+    protected array $columns = [
+        'goals',
+        'assists',
+        'points',
+        'shots',
+        'hits',
+        'pp_goals',
+        'pp_assists',
+        'pp_points',
+        'pim',
+        'fo_wins',
+        'fo_taken',
+        'takeaways',
+        'giveaways',
+        'sh_goals',
+        'sh_assists',
+        'sh_points',
+        'blocked_shots',
+    ];
+
     public function save(): void
     {
-        dd('ath3');
+        $stats = GameTeamStats::select([
+            'game_type_id',
+            'team_id',
+            DB::raw('COUNT(*) as games_played'),
+            ...collect($this->columns)->map(function($s) {
+                return DB::raw("SUM({$s}) as {$s}");
+            })
+        ])->groupBy(['game_type_id', 'team_id'])
+          ->get();
+
+        AlltimeTeamStats::truncate();
+        $stats->chunk(100)->each(function($data): void {
+            AlltimeTeamStats::insert($data->toArray());
+        });
     }
 }
